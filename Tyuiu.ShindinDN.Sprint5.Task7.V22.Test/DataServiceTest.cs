4b294ad97355625714aa3cfb696c0cb79f93c5a0
ShindinDenis <viber.kat@outlook.com>
2024-11-27 00:51:41 +0500

    Task7

---
+++ Tyuiu.ShindinDN.Sprint5.Task7.V22.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.ShindinDN.Sprint5.Task7.V22.Lib;
+
+namespace Tyuiu.ShindinDN.Sprint5.Task7.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.Combine("C:", "DataSprint5", "InPutDataFileTask7V22.txt");
+            DataService ds = new DataService();
+            var res = ds.LoadDataAndSave(path);
+            string wait = "Ст#ока# и# #точка#";
+            Assert.AreEqual(wait, File.ReadAllText(res));
+        }
+    }
+}
