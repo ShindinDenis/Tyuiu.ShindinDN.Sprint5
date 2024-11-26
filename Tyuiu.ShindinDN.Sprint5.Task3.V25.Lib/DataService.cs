7738993a9810dfbca66591cdab813e88b601c80d
ShindinDenis <viber.kat@outlook.com>
2024-11-26 22:48:13 +0500

    Task3

---
+++ Tyuiu.ShindinDN.Sprint5.Task3.V25.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.ShindinDN.Sprint5.Task3.V25.Lib
+{
+    public class DataService : ISprint5Task3V25
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+            double y = Math.Round((3*Math.Pow(x,4)+1)/Math.Pow(x,3),3);
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.ShindinDN.Sprint5.Task3.V25.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using Tyuiu.ShindinDN.Sprint5.Task3.V25.Lib;
+
+namespace Tyuiu.ShindinDN.Sprint5.Task3.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+
+        }
+    }
+}
